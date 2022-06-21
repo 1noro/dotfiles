a3670e1f872bf9920cbb0f840d06170ae08a4f9e
1noro <ppuubblliicc@protonmail.com>
2022-06-21 19:58:10 +0200

    feat: :sparkles: perfeccionado deploy process

--- Makefile
+++ Makefile
@@ -1,26 +1,20 @@
-
 .PHONY: all
-all: deps deploy
+all: help
+
+##@ Generic utils
+.PHONY: help
+help: ## Print help
+	@awk 'BEGIN {FS = ":.*##"; printf "Usage:\n  make \033[36m<target>\033[0m\n"} /^[^{]+:.*?##/ { printf "  \033[36m%-23s\033[0m %s\n", $$1, $$2 } /^##@/ { printf "\n\033[1m%s\033[0m\n", substr($$0, 5) } ' $(MAKEFILE_LIST)
 
 .PHONY: deps
-deps:
+deps: ## Install dependencies
 	@echo "installing deps"
 
 .PHONY: deploy
-deploy:
-	# @/bin/bash scripts/deploy.sh
-	@echo "deploying"
+deploy: ## Deploy configs
+	@/bin/bash scripts/deploy.sh
 
 .PHONY: clean
-clean:
+clean: ## Clean configs
 	# @/bin/bash scripts/reset.sh
 	@echo "cleaning"
-
-.PHONY: help
-help:
-	@echo "make"
-	@echo "make deps"
-	@echo "make deploy"
-	@echo "make clean"
-	@echo "make help"
-
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+set -e
+
+# previous config backup directory
 BKP_DIR="$HOME/.dotfiles-bkp"
+mkdir -p "$BKP_DIR"
 
-mkdir -p $BKP_DIR
-
+# bash
 if [ "$SHELL" = "/bin/bash" ]; then
     mv "$HOME/.bash_profile" "$BKP_DIR/"
     ln -s "$PWD/.bash_profile" "$HOME/"
@@ -11,17 +14,48 @@ if [ "$SHELL" = "/bin/bash" ]; then
     ln -s "$PWD/.bashrc" "$HOME/"
 fi
 
+# zsh
 if [ "$SHELL" = "/usr/bin/zsh" ]; then
     mv "$HOME/.zshrc" "$BKP_DIR/"
     ln -s "$PWD/.zshrc" "$HOME/"
 fi
 
-if [ -f "$HOME/.vimrc" ]; then
-    mv "$HOME/.vimrc" "$BKP_DIR/"
-    ln -s "$PWD/.vimrc" "$HOME/"
-    mkdir -p "$HOME/.vim/ftplugin/"
-    mkdir -p "$HOME/dotfiles-bkp/.vim/ftplugin/"
-    mv "$HOME/.vim/ftplugin/*" "$HOME/dotfiles-bkp/.vim/ftplugin/" 2> /dev/null
-    ln -s "$PWD/.vim/ftplugin/*" "$HOME/.vim/ftplugin/"
+# git
+if [ -f "$HOME/.gitconfig" ]; then
+    mv "$HOME/.gitconfig" "$BKP_DIR/"
+    ln -s "$PWD/.gitconfig" "$HOME/"
 fi
 
+# vim
+# if [ -f "$HOME/.vimrc" ]; then
+#     mv "$HOME/.vimrc" "$BKP_DIR/"
+#     ln -s "$PWD/.vimrc" "$HOME/"
+#     mkdir -p "$HOME/.vim/ftplugin/"
+#     mkdir -p "$HOME/dotfiles-bkp/.vim/ftplugin/"
+#     mv "$HOME/.vim/ftplugin/*" "$HOME/dotfiles-bkp/.vim/ftplugin/" 2> /dev/null
+#     ln -s "$PWD/.vim/ftplugin/*" "$HOME/.vim/ftplugin/"
+# fi
+
+# nvim
+if [ -d "$HOME/.config/nvim" ]; then
+    mv "$HOME/.config/nvim" "$BKP_DIR/.config/"
+    ln -s "$PWD/.config/nvim" "$HOME/.config/"
+fi
+
+#beets
+if [ -d "$HOME/.config/beets" ]; then
+    mv "$HOME/.config/beets" "$BKP_DIR/.config/"
+    ln -s "$PWD/.config/beets" "$HOME/.config/"
+fi
+
+#mpv
+if [ -d "$HOME/.config/mpv" ]; then
+    mv "$HOME/.config/mpv" "$BKP_DIR/.config/"
+    ln -s "$PWD/.config/mpv" "$HOME/.config/"
+fi
+
+# starship
+if [ -f "$HOME/.config/starship.toml" ]; then
+    mv "$HOME/.config/starship.toml" "$BKP_DIR/.config/"
+    ln -s "$PWD/.config/starship.toml" "$HOME/.config/"
+fi
